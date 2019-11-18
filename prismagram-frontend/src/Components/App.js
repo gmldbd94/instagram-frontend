e6345b8336aa5c0e2fe2a0b1405ee54fa078bbf8
gmldbd94 <gmldbd@likelion.org>
2019-11-18 19:46:08 +0900

    apollo setting for Auth

--- prismagram-frontend/src/Components/App.js
+++ prismagram-frontend/src/Components/App.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { gql } from "apollo-boost";
 import GlobalStyles from "../Styles/GlobalStyles";
 import {ThemeProvider} from "styled-components";
 import Theme from "../Styles/Theme";
 import Router from "./Router"
-export default () => (
-  <ThemeProvider theme={Theme}>
-  <>
-    <GlobalStyles />
-    <Router isLoggedIn={false} />
-  </>
-  </ThemeProvider>
-);
+import { useQuery } from "react-apollo-hooks";
+
+const QUERY= gql `
+  {
+    isLoggedIn @client
+  }
+`
+
+export default () => {
+
+  const { data : {isLoggedIn} } = useQuery(QUERY);
+
+  return(
+    <ThemeProvider theme={Theme}>
+      <>
+        <GlobalStyles />
+        <Router isLoggedIn={isLoggedIn} />
+      </>
+    </ThemeProvider>
+  );
+};
