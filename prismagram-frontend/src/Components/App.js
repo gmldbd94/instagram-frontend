e4000cf22abcd525ce4990e466fdd1de925ec7c7
gmldbd94 <gmldbd@likelion.org>
2019-11-22 21:59:37 +0900

    authentication LOG_IN complete

--- prismagram-frontend/src/Components/App.js
+++ prismagram-frontend/src/Components/App.js
@@ -1,16 +1,32 @@
 import React from "react";
 import { gql } from "apollo-boost";
-import GlobalStyles from "../Styles/GlobalStyles";
-import {ThemeProvider} from "styled-components";
-import Theme from "../Styles/Theme";
-import Router from "./Router"
 import { useQuery } from "react-apollo-hooks";
+
+//토스트메세지를 위한 라이브러리
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+//스타일 구성
+import { HashRouter as Router } from "react-router-dom";
+import GlobalStyles from "../Styles/GlobalStyles";
+import styled, {ThemeProvider} from "styled-components";
+import Theme from "../Styles/Theme";
+import Routes from "./Router"
+import Header from "./Header";
+import Footer from "./Footer";
 
 const QUERY= gql `
   {
     isLoggedIn @client
   }
-`
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: ${props => props.theme.maxWidth};
+  width: 100%;
+`;
+
 
 export default () => {
 
@@ -19,8 +35,16 @@ export default () => {
   return(
     <ThemeProvider theme={Theme}>
       <>
-        <GlobalStyles />
-        <Router isLoggedIn={isLoggedIn} />
+        <Router>
+          <>
+            <Header />
+            <Wrapper>
+              <Routes isLoggedIn={isLoggedIn} />
+              <Footer />
+            </Wrapper>
+          </>
+        </Router>
+        <ToastContainer position={toast.POSITION.BOTTOM_LEFT}/>
       </>
     </ThemeProvider>
   );
