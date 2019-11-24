a5686cd90a399b3010af399c98daa51853dc613e
gmldbd94 <gmldbd@likelion.org>
2019-11-24 16:45:54 +0900

    loading Feed Info complete

---
+++ prismagram-frontend/src/Components/Post/PostContainer.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import useInput from "../../Hooks/useInput";
+import PostPresenter from "./PostPresenter";
+
+const PostContainer = ({
+  id,
+  user,
+  files,
+  likeCount,
+  isLiked,
+  comments,
+  createdAt
+}) => {
+  return <PostPresenter />;
+};
+//값을 받아오는 형태를 지정해준다.
+//gpl의 query를 확인해볼것!
+PostContainer.propTypes = {
+  id: PropTypes.string.isRequired,
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+    username: PropTypes.string.isRequired
+  }).isRequired,
+  files: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired
+    })
+  ).isRequired,
+  likeCount: PropTypes.number.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      user: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        username: PropTypes.string.isRequired
+      }).isRequired
+    })
+  ).isRequired,
+  createdAt: PropTypes.string
+};
+
+export default PostContainer;
--- prismagram-frontend/src/Routes/Feed.js
+++ prismagram-frontend/src/Routes/Feed.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { gql } from "apollo-boost";
 import { useQuery } from "react-apollo-hooks";
 import Loader from "../Components/Loader";
-
+import Post from "../Components/Post";
 //Feed에 해당하는 gql
 const FEED_QUERY = gql`
   {
@@ -46,5 +46,24 @@ export default () => {
   const { data, loading } = useQuery(FEED_QUERY);
   //loading 하는동안의 recycle view를 표시해준다.
   console.log(data);
-  return <Wrapper>{loading && <Loader />}</Wrapper>;
+  return (
+    <Wrapper>
+      {loading && <Loader />}
+      {!loading &&
+        data &&
+        data.seeFeed &&
+        data.seeFeed.map(post => (
+          <Post
+            key={post.id}
+            id={post.id}
+            user={post.user}
+            files={post.files}
+            likeCount={post.likeCount}
+            isLiked={post.isLiked}
+            comments={post.comments}
+            createdAt={post.createdAt}
+          />
+        ))}
+    </Wrapper>
+  );
 };
